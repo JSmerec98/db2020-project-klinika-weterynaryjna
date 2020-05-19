2b55c045589368eb247ee746eda2eddf3b90575b
Jan Smerecki <57664542+JSmerec98@users.noreply.github.com>
2020-05-19 21:16:56 +0200

    Updated vet_clinic.sql

--- src/sql/vet_clinic.sql
+++ src/sql/vet_clinic.sql
@@ -2,8 +2,8 @@
 -- version 5.0.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Czas generowania: 12 Maj 2020, 18:43
+-- Host: localhost
+-- Czas generowania: 19 Maj 2020, 21:07
 -- Wersja serwera: 10.4.11-MariaDB
 -- Wersja PHP: 7.4.3
 
@@ -73,7 +73,8 @@ CREATE TABLE `animal_diagnosis` (
 CREATE TABLE `appointment` (
   `appointment_id` int(11) NOT NULL,
   `data` date NOT NULL,
-  `time` time NOT NULL
+  `time` time NOT NULL,
+  `veterinarian_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -91,11 +92,61 @@ CREATE TABLE `diagnosis` (
 -- --------------------------------------------------------
 
 --
+-- Struktura tabeli dla tabeli `drug`
+--
+
+CREATE TABLE `drug` (
+  `drug_id` int(11) NOT NULL,
+  `drug_name` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `drug_plan`
+--
+
+CREATE TABLE `drug_plan` (
+  `drug_plan_id` int(11) NOT NULL,
+  `diagnosis_id` int(11) NOT NULL,
+  `advices` varchar(255) DEFAULT NULL,
+  `drug_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Struktura tabeli dla tabeli `owner`
 --
 
 CREATE TABLE `owner` (
   `owner_id` int(11) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `phone_number` int(9) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `prescription`
+--
+
+CREATE TABLE `prescription` (
+  `perscription_id` int(11) NOT NULL,
+  `veterinarian_id` int(11) NOT NULL,
+  `drug_plan_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `veterinarian`
+--
+
+CREATE TABLE `veterinarian` (
+  `veterinarian_id` int(11) NOT NULL,
   `first_name` varchar(50) NOT NULL,
   `last_name` varchar(50) NOT NULL,
   `address` varchar(100) NOT NULL,
@@ -132,7 +183,8 @@ ALTER TABLE `animal_diagnosis`
 -- Indeksy dla tabeli `appointment`
 --
 ALTER TABLE `appointment`
-  ADD PRIMARY KEY (`appointment_id`);
+  ADD PRIMARY KEY (`appointment_id`),
+  ADD KEY `veterinarian_id` (`veterinarian_id`);
 
 --
 -- Indeksy dla tabeli `diagnosis`
@@ -141,10 +193,38 @@ ALTER TABLE `diagnosis`
   ADD PRIMARY KEY (`diagnosis_id`);
 
 --
+-- Indeksy dla tabeli `drug`
+--
+ALTER TABLE `drug`
+  ADD PRIMARY KEY (`drug_id`);
+
+--
+-- Indeksy dla tabeli `drug_plan`
+--
+ALTER TABLE `drug_plan`
+  ADD PRIMARY KEY (`drug_plan_id`),
+  ADD KEY `diagnosis_id` (`diagnosis_id`),
+  ADD KEY `drug_id` (`drug_id`);
+
+--
 -- Indeksy dla tabeli `owner`
 --
 ALTER TABLE `owner`
   ADD PRIMARY KEY (`owner_id`);
+
+--
+-- Indeksy dla tabeli `prescription`
+--
+ALTER TABLE `prescription`
+  ADD PRIMARY KEY (`perscription_id`),
+  ADD KEY `veterinarian_id` (`veterinarian_id`),
+  ADD KEY `drug_plan_id` (`drug_plan_id`);
+
+--
+-- Indeksy dla tabeli `veterinarian`
+--
+ALTER TABLE `veterinarian`
+  ADD PRIMARY KEY (`veterinarian_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -197,6 +277,26 @@ ALTER TABLE `animal_appointment`
 ALTER TABLE `animal_diagnosis`
   ADD CONSTRAINT `animal_diagnosis_ibfk_1` FOREIGN KEY (`appointment_id`) REFERENCES `appointment` (`appointment_id`),
   ADD CONSTRAINT `animal_diagnosis_ibfk_2` FOREIGN KEY (`diagnosis_id`) REFERENCES `diagnosis` (`diagnosis_id`);
+
+--
+-- Ograniczenia dla tabeli `appointment`
+--
+ALTER TABLE `appointment`
+  ADD CONSTRAINT `appointment_ibfk_1` FOREIGN KEY (`veterinarian_id`) REFERENCES `veterinarian` (`veterinarian_id`);
+
+--
+-- Ograniczenia dla tabeli `drug_plan`
+--
+ALTER TABLE `drug_plan`
+  ADD CONSTRAINT `drug_plan_ibfk_1` FOREIGN KEY (`diagnosis_id`) REFERENCES `diagnosis` (`diagnosis_id`),
+  ADD CONSTRAINT `drug_plan_ibfk_2` FOREIGN KEY (`drug_id`) REFERENCES `drug` (`drug_id`);
+
+--
+-- Ograniczenia dla tabeli `prescription`
+--
+ALTER TABLE `prescription`
+  ADD CONSTRAINT `prescription_ibfk_1` FOREIGN KEY (`veterinarian_id`) REFERENCES `veterinarian` (`veterinarian_id`),
+  ADD CONSTRAINT `prescription_ibfk_2` FOREIGN KEY (`drug_plan_id`) REFERENCES `drug_plan` (`drug_plan_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
